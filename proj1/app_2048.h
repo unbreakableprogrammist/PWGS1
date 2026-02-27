53af386af7d9a54b73d97a9fc62866866c369a19
Krzysztof <criskbp.2019@gmail.com>
2026-02-27 17:45:58 +0100

    kocham Legie

--- proj1/app_2048.h
+++ proj1/app_2048.h
@@ -11,9 +11,18 @@ private:
 	// wstring - jest to typ danych reprezentujacy lancuch znakow szerokich (wide string), czyli taki ktory moze przechowywac znaki z wiekszej ilosci jezykow, np polskie znaki diakrytyczne
 	//const - oznacza ze ta zmienna jest stala, czyli jej wartosc nie moze byc zmieniona po jej zainicjalizowaniu
 	static wstring const s_class_name;
-
+	// ogolna funkcja ktora wywluje windows gdy jest jakies zdarzenie zwiazane z oknem, np klikniecie myszka, nacisniecie klawisza, itp
 	static LRESULT CALLBACK window_proc_static(
-
-	);
+		HWND window, // uchwyt do okna, czyli identyfikator okna, ktory jest przekazywany przez system operacyjny do funkcji obslugi zdarzen, gdy wystapi zdarzenie zwiazane z tym oknem
+		UINT message, // kod zdarzenia, czyli liczba calkowita reprezentujaca rodzaj zdarzenia, np klikniecie myszka, nacisniecie klawisza, itp
+		WPARAM wparam, // dodatkowe informacje o zdarzeniu, ktore sa przekazywane jako argumenty do funkcji obslugi zdarzen, np kod klawisza, pozycja kursora, itp
+		LPARAM lparam // dodatkowe informacje o zdarzeniu, ktore sa przekazywane jako argumenty do funkcji obslugi zdarzen, np kod klawisza, pozycja kursora, itp
+		);
+	HWND create_window(); // deklarujemy funkcje create_window, ktora bedzie odpowiedzialna za tworzenie okna gry
+	HINSTANCE m_instance; // deklarujemy zmienna m_instance, ktora bedzie przechowywac uchwyt do instancji aplikacji, czyli identyfikator procesu, ktory jest przekazywany przez system operacyjny do funkcji wWinMain, gdy aplikacja jest uruchamiana
+	HWND m_main; // deklarujemy zmienna m_main, ktora bedzie przechowywac uchwyt do glownego okna gry, czyli identyfikator okna, ktory jest zwracany przez funkcje create_window, gdy okno jest tworzone
+public:
+	app_2048(HINSTANCE instance); // deklarujemy konstruktor klasy app_2048, ktory bedzie odpowiedzialny za inicjalizacje aplikacji, czyli rejestracje klasy okna, tworzenie glownego okna gry, itp
+	int run(int show_command); // deklarujemy funkcje run, ktora bedzie odpowiedzialna za uruchomienie glownej petli zdarzen gry, czyli odbieranie i obsluge zdarzen zwiazanych z oknem gry, itp
 };
 
